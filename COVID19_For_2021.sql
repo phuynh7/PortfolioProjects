afb049964984ef1516e966bffd6d9a6296d55a46
phuynh7 <74264424+phuynh7@users.noreply.github.com>
2023-01-29 19:36:15 -0600

    Update COVID19_For_2021.sql

--- COVID19_For_2021.sql
+++ COVID19_For_2021.sql
@@ -1,3 +1,10 @@
+/*
+Covid 19 Data Exploration with the Data of 2021
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
+
 select *
 from PortfolioProject..CovidDeaths
 where continent is not null
